dbc15f5c5c1c47b0928f99f340b057ac00ce9c7c
Maxim <serieznyi@gmail.com>
2018-08-30 23:18:47 +0300

    первый набросок нового ui на javafx

---
+++ src/main/java/org/cyberbiology/App.java
@@ -0,0 +1,21 @@
+package org.cyberbiology;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../../fxml/sample.fxml"));
+        primaryStage.setTitle("CyberBiologyTest 1.0.0");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/org/cyberbiology/Controller.java
@@ -0,0 +1,11 @@
+package org.cyberbiology;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuItem;
+
+public class Controller {
+    public MenuItem menuButtonRun;
+
+    public void actionStartSimulation(ActionEvent actionEvent) {
+    }
+}
