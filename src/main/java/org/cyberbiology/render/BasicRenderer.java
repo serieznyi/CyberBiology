dd4f5c926b65947cfcfca7066a710373e4f7fc00
Maxim <serieznyi@gmail.com>
2018-09-12 09:40:03 +0300

    небольшой рефакторинг renderer

--- src/main/java/org/cyberbiology/render/BasicRenderer.java
+++ src/main/java/org/cyberbiology/render/BasicRenderer.java
@@ -2,6 +2,7 @@ package org.cyberbiology.render;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import org.cyberbiology.Bot;
 import org.cyberbiology.World;
 import org.cyberbiology.prototype.view.IRenderer;
 
@@ -17,25 +18,42 @@ public class BasicRenderer implements IRenderer
 
         for (int y = 0; y < world.height; y++) {
             for (int x = 0; x < world.width; x++) {
-                if (world.matrix[x][y] == null) {
+                Bot bot = world.matrix[x][y];
+
+                if (bot == null) {
                     graphicsContext.setFill(Color.WHITE);
                     graphicsContext.fillRect(x * World.BOT_WIDTH,y * World.BOT_HEIGHT, World.BOT_WIDTH, World.BOT_HEIGHT);
-                } else if ((world.matrix[x][y].alive == 1) || (world.matrix[x][y].alive == 2)) {
+                } else if (bot.alive == 1 || bot.alive == 2) {
                     graphicsContext.setFill(Color.rgb(200, 200, 200));
                     graphicsContext.fillRect(x * World.BOT_WIDTH, y * World.BOT_HEIGHT, World.BOT_WIDTH, World.BOT_HEIGHT);
                     world.organic = world.organic + 1;
-                } else if (world.matrix[x][y].alive == 3) {
+                } else if (bot.alive == 3) {
                     graphicsContext.setFill(Color.BLACK);
                     graphicsContext.fillRect(x * World.BOT_WIDTH, y * World.BOT_HEIGHT, World.BOT_WIDTH, World.BOT_HEIGHT);
 
-                    int green = world.matrix[x][y].c_green - ((world.matrix[x][y].c_green * world.matrix[x][y].health) / 2000);
-                    if (green < 0) green = 0;
-                    if (green > 255) green = 255;
-                    int blue = (int) (world.matrix[x][y].c_blue * 0.8 - ((world.matrix[x][y].c_blue * world.matrix[x][y].mineral) / 2000));
-                    graphicsContext.setFill(Color.rgb(world.matrix[x][y].c_red, green, blue));
+                    Color botColor = this.calculateBotColor(bot);
+
+                    graphicsContext.setFill(botColor);
+
                     graphicsContext.fillRect(x * World.BOT_WIDTH + 1, y * World.BOT_HEIGHT + 1,World.BOT_WIDTH -1, World.BOT_HEIGHT -1);
                 }
             }
         }
+    }
+
+    private Color calculateBotColor(Bot bot) {
+        int green = bot.c_green - ((bot.c_green * bot.health) / 2000);
+
+        if (green < 0)  {
+            green = 0;
+        }
+
+        if (green > 255) {
+            green = 255;
+        }
+
+        int blue = (int) (bot.c_blue * 0.8 - ((bot.c_blue * bot.mineral) / 2000));
+
+        return Color.rgb(bot.c_red, green, blue);
     }
 }
